6bea39ea1233d00902116fa31785a59fa3bfcf19
Kevin Wang <bestboykevin.bbg@gmail.com>
2020-06-03 21:38:06 +0200

    Initial commit

---
+++ blackjack.py
@@ -0,0 +1,204 @@
+"""Some of the code is inspired by various people online."""
+
+from random import randint
+
+cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+cardValues = {
+    'A': 11, 'a': 1,
+    '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+    'J': 10, 'Q': 10, 'K': 10
+}
+cardCounter = {}
+
+
+def drawCard():
+    """There are 8 decks in one game of blackjack, and four of the same value cards in one deck."""
+    for card in cards:
+        cardCounter[card] = 32
+
+    available = True
+    while available:
+        card = cards[randint(0, 12)]
+        if cardCounter[card] <= 0:
+            cardCounter[card] = cardCounter[card] - 1
+        else:
+            available = False
+    return card
+
+
+# def playerFirst():
+#     pDeck = []
+#     pDeck.append(drawCard())
+#     pDeck.append(drawCard())
+#     if
+
+def player():
+    pDeck = []
+    pChoice = "hit"
+    pDeck.append(drawCard())
+    while pChoice == "hit":
+        pDeck.append(drawCard())
+        pDeck = checkAce(pDeck)
+        print("Your current deck is:")
+        for card in pDeck:
+            print(card, end=' ')
+        print("\n\n")
+        if checkBust(pDeck):
+            break
+        if checkBlackjack(pDeck):
+            break
+        pChoice = input("Do you want to hit or stand?\n> ")
+    return pDeck
+
+
+def dealerFirst():
+    dDeck = [drawCard(), drawCard()]
+    return dDeck
+
+
+def dealerAfter(dDeck):
+    while getValue(dDeck) < 17:
+        dDeck.append(drawCard())
+        dDeck = checkAce(dDeck)
+    return dDeck
+
+
+def getValue(deck):
+    value = 0
+    for card in deck:
+        value += cardValues[card]
+    return value
+
+
+def checkAce(deck):
+    value = getValue(deck)
+    for card in deck:
+        if cardValues[card] == 11 and value > 21:
+            acePos = deck.index("A")
+            deck[acePos] = "a"
+    return deck
+
+
+def checkBust(deck):
+    if getValue(deck) > 21:
+        print("You bust!")
+        print("Your value was", getValue(deck))
+        return True
+
+
+def checkBlackjack(deck):
+    if getValue(deck) == 21:
+        return True
+
+
+def deckCompare(pDeck, dDeck):
+    dealerWin = False
+    playerWin = False
+    pValue = getValue(pDeck)
+    dValue = getValue(dDeck)
+    if checkBust(pDeck): #  If player busts.
+        dealerWin = True
+    elif checkBust(dDeck): # Else, if dealer busts
+        playerWin = True
+
+    elif checkBlackjack(pDeck): # If player has blackjack
+        if checkBlackjack(dDeck): #  If dealer also has blackjack
+            dealerWin = False
+            playerWin = False
+        else:
+            playerWin = True
+
+    elif dValue < 21 and pValue < 21:
+        if dValue > pValue:
+            dealerWin = True
+        elif pValue > dValue:
+            playerWin = True
+        elif pValue == dValue:
+            playerWin = False
+            dealerWin = False
+
+    return dealerWin, playerWin
+
+def win_decider(dealerWin, playerWin):
+    if dealerWin == True:
+        print("The dealer has won!")
+    elif playerWin == True:
+        print("Congratulations, you won!")
+    elif playerWin == False and dealerWin == False:
+        print("Both hands were the same, it's a push.")
+
+
+def game():
+    dDeck = dealerFirst()
+    print("The dealer's card is", dealerFirst()[0], "\n")
+    pDeck = player()
+    dealerAfter(dDeck)
+    dealerWin, playerWin = deckCompare(pDeck, dDeck)
+    win_decider(dealerWin, playerWin)
+
+
+def help():
+    print("\n\n\n\n\n\n\n\nHello!")
+    print("You are playing blackjack, the goal is to have the total value of your cards higher than the dealer's cards")
+    print("without going over 21.")
+    ruleNumber = input("What would you like more information about?\n"
+                       "  1. Cards and their values\n"
+                       "  2. Commands\n"
+                       "  3. Rules for the dealer\n"
+                       "  4. Back\n"
+                       "  > "
+                       )
+    print("\n\n\n\n\n\n\n")
+    if ruleNumber == '1':
+        helpCards()
+    elif ruleNumber == '2':
+        helpCommands()
+    elif ruleNumber == '3':
+        helpDealerRules()
+    else:
+        main()
+
+
+def helpCards():
+    print("These are all the values of cards:\n")
+    for card, value in cardValues.items():
+        print(card, ",", value)
+        print("\nNote: An Ace can have either a value of 1, or 11. Its value is 11 if the total value of the deck\n"
+              "is 21 or lower, also called; a 'soft' deck.\n"
+              "If the total value of a deck is over 21, the Ace will have a value of 1.")
+
+
+def helpCommands():
+    print("Commands:\n"
+          " In blackjack, you can 'hit', or 'stand'.\n"
+          " Hitting means that the player will pick another card, and thus"
+          " adding more value to their deck.\n"
+          " Staning means that the player will stop drawing cards and the dealer will unveil their other card.")
+
+
+def helpDealerRules():
+    print("Dealer's rules:\n"
+          " 1. A dealer only shows their first drawn card.\n"
+          " 2. A dealer has to keep hitting until the value of their deck is 17 or above."
+          )
+
+
+def main():
+    menuOption = input("Select an option:\n"
+                       "  1. Start\n"
+                       "  2. Help/Rules\n"
+                       "  3. Quit\n"
+                       "  > "
+                       )
+    if menuOption == '1':
+        game()
+    elif menuOption == '2':
+        help()
+    elif menuOption == '3':
+        exit()
+    else:
+        print("That's not even an option, but I'll just quit then...\n")
+
+
+if __name__ == "__main__":
+    main()
