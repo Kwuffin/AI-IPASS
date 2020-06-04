01c79bdb9901994789e48a1927ad5bc6716001de
Kevin Wang <bestboykevin.bbg@gmail.com>
2020-06-04 22:17:54 +0200

    Edited player()-function to be compatible with splitting.
    Splitting will be introduced in later versions

--- blackjack.py
+++ blackjack.py
@@ -10,11 +10,11 @@ cardValues = {
 }
 cardCounter = {}
 
+"""There are 8 decks in one game of blackjack, and four of the same value cards in one deck."""
+for card in cards:
+    cardCounter[card] = 32
 
 def drawCard():
-    """There are 8 decks in one game of blackjack, and four of the same value cards in one deck."""
-    for card in cards:
-        cardCounter[card] = 32
 
     available = True
     while available:
@@ -26,29 +26,46 @@ def drawCard():
     return card
 
 
-# def playerFirst():
-#     pDeck = []
-#     pDeck.append(drawCard())
-#     pDeck.append(drawCard())
-#     if
+def split(deck):
+    deck1 = []
+    deck2 = []
+    allDecks = [deck1, deck2]
+    deck1.append(deck[0])
+    deck2.append(deck[1])
+    print("Splitting deck:")
+    print("Deck 1:", deck1)
+    print("Deck 2:", deck2)
+    print("\n\n")
+    for deck in allDecks:
+        print("Current hand:", deck1[0])
+        pChoice = input("")
+
 
 def player():
     pDeck = []
-    pChoice = "hit"
     pDeck.append(drawCard())
+    pChoice = "hit"
     while pChoice == "hit":
         pDeck.append(drawCard())
         pDeck = checkAce(pDeck)
-        print("Your current deck is:")
+        print("Player:")
         for card in pDeck:
-            print(card, end=' ')
-        print("\n\n")
+            print(card, end= ' ')
+        print("\nTotal:", getValue(pDeck))
         if checkBust(pDeck):
             break
         if checkBlackjack(pDeck):
             break
-        pChoice = input("Do you want to hit or stand?\n> ")
-    return pDeck
+        print("          =============================================================================================")
+        if pDeck[0] == pDeck[1]:
+            pChoice = input("Would you like to hit, stand or split?\n> ")
+        else:
+            pChoice = input("Would you like to hit or stand?\n> ")
+        pChoice.lower()
+    if pChoice == "split":
+        split(pDeck)
+    else:
+        return pDeck
 
 
 def dealerFirst():
@@ -58,8 +75,13 @@ def dealerFirst():
 
 def dealerAfter(dDeck):
     while getValue(dDeck) < 17:
-        dDeck.append(drawCard())
+        print("Dealer:\n"
+              "Value:", getValue(dDeck))
+        newCard = drawCard()
+        dDeck.append(newCard)
         dDeck = checkAce(dDeck)
+        print("\nDraws", newCard)
+        print("Value:", getValue(dDeck))
     return dDeck
 
 
@@ -81,8 +103,6 @@ def checkAce(deck):
 
 def checkBust(deck):
     if getValue(deck) > 21:
-        print("You bust!")
-        print("Your value was", getValue(deck))
         return True
 
 
@@ -98,43 +118,59 @@ def deckCompare(pDeck, dDeck):
     dValue = getValue(dDeck)
     if checkBust(pDeck): #  If player busts.
         dealerWin = True
+        print("You bust!")
+        print("Your value was", getValue(pDeck))
     elif checkBust(dDeck): # Else, if dealer busts
         playerWin = True
+        print("The dealer bust!")
+        print("Their value was", getValue(dDeck))
 
     elif checkBlackjack(pDeck): # If player has blackjack
         if checkBlackjack(dDeck): #  If dealer also has blackjack
             dealerWin = False
             playerWin = False
+            print("Both had blackjack!")
         else:
             playerWin = True
+            print("Blackjack!")
 
     elif dValue < 21 and pValue < 21:
         if dValue > pValue:
             dealerWin = True
+            print("The dealer's value is higher than yours.")
+            print("Dealer:", getValue(dDeck))
+            print("Player:", getValue(pDeck))
         elif pValue > dValue:
             playerWin = True
+            print("The player's value is higher than the dealer's!")
+            print("Dealer:", getValue(dDeck))
+            print("Player:", getValue(pDeck))
         elif pValue == dValue:
             playerWin = False
             dealerWin = False
+            print("Both hands were the same, it's a push.")
+            print("Dealer:", getValue(dDeck))
+            print("Player:", getValue(pDeck))
 
     return dealerWin, playerWin
 
-def win_decider(dealerWin, playerWin):
+
+def win_decide(dealerWin, playerWin):
+    print("=============================================================================================")
+    print("\n")
     if dealerWin == True:
-        print("The dealer has won!")
+        print("The dealer has won.")
     elif playerWin == True:
         print("Congratulations, you won!")
-    elif playerWin == False and dealerWin == False:
-        print("Both hands were the same, it's a push.")
 
 
 def game():
     dDeck = dealerFirst()
-    print("The dealer's card is", dealerFirst()[0], "\n")
+    print("Dealer:\n", dDeck[0], "\n")
     pDeck = player()
     dealerAfter(dDeck)
     dealerWin, playerWin = deckCompare(pDeck, dDeck)
-    win_decider(dealerWin, playerWin)
+    win_decide(dealerWin, playerWin)
 
 
 def help():
@@ -162,10 +198,12 @@ def help():
 def helpCards():
     print("These are all the values of cards:\n")
     for card, value in cardValues.items():
-        print(card, ",", value)
-        print("\nNote: An Ace can have either a value of 1, or 11. Its value is 11 if the total value of the deck\n"
-              "is 21 or lower, also called; a 'soft' deck.\n"
-              "If the total value of a deck is over 21, the Ace will have a value of 1.")
+        print("{:2}  -  {:2}".format(card, value))
+    print("\nNote: An Ace can have either a value of 1, or 11. Its value is 11 if the total value of the deck\n"
+            "is 21 or lower, also called; a 'soft' deck.\n"
+            "If the total value of a deck is over 21, the Ace will have a value of 1.\n"
+            "A soft deck with an ace (11) will be visualized as a capital 'A', and a hard deck with an ace will\n"
+            "be visualized as an uncapitalized 'a'.")
 
 
 def helpCommands():
@@ -191,6 +229,7 @@ def main():
                        "  > "
                        )
     if menuOption == '1':
+        print("==================================║ Welcome to blackjack! ║==================================")
         game()
     elif menuOption == '2':
         help()
@@ -198,6 +237,7 @@ def main():
         exit()
     else:
         print("That's not even an option, but I'll just quit then...\n")
+        exit()
 
 
 if __name__ == "__main__":
